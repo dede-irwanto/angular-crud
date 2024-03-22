8ee0783479a42d0367ef76bc342ecaf15d43176b
dede-irwanto <id.dedeirwanto@gmail.com>
2024-03-22 20:36:04 +0800

    add api service

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -17,6 +17,7 @@ import {
   FormsModule,
 } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-dialog',
@@ -44,7 +45,7 @@ export class DialogComponent implements OnInit {
 
   productForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private api : ApiService) {}
 
   ngOnInit(): void {
     this.productForm = this.formBuilder.group({
@@ -58,6 +59,16 @@ export class DialogComponent implements OnInit {
   }
 
   addProduct() {
-    console.log(this.productForm.value);
+    if(this.productForm.valid) {
+      this.api.postProduct(this.productForm.value)
+      .subscribe({
+        next:(res)=>{
+          alert("Product added successfully")
+        },
+        error:()=>{
+          alert("Error while adding the product")
+        }
+      })
+    }
   }
 }
